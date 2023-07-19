669a6220005abcb9c207cefd2ceb9ed3241f7af5
Stefan Schaller <stefan.schaller@tapped.dev>
2023-07-19 10:17:26 +0200

    set cors settings

--- server.js
+++ server.js
@@ -25,7 +25,7 @@
 
 const express = require('express');
 const fs = require('fs');
-const http = require('http');
+const https = require('https');
 
 // Setting up the private key and the certificate
 // ==============================================
@@ -140,14 +140,10 @@ app.get('/authenticate', (req, res) => {
 })
 
 // Let's create our HTTPS server and we're ready to go.
-const server = http.createServer(opts, app)
+const server = https.createServer(opts, app)
     .listen(PORT, () => {
             console.log(`Server is running in port: ${PORT}`)
          });
-
-/*app.listen(PORT, () => {
-	console.log(`Server is running in port: ${PORT}`)
-});*/
 
 
 // Then we can start our server with `npm i && node server.js`.
